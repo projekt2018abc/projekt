fb5cc10810ff383af9e03cd06806804608b9eacb
GZych <zbchcz@gmail.com>
2018-06-06 21:09:23 +0200

    Dodano Context do kontaktu z bazą danych. Trzeba skonfigurować options!

--- WebApplication6/ProjektZespolowyContext.cs
+++ WebApplication6/ProjektZespolowyContext.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProjektZespolowy.Models;
 
 namespace ProjektZespolowy
 {
-    public class ProjektZespolowyContext
+    public class ProjektZespolowyContext : DbContext
     {
+        DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder<ProjektZespolowyContext>();
+        
+
+        public DbSet<Osoba> Osoby { get; set; }
+        public DbSet<Klient> Klienci { get; set; }
+
+
+        public ProjektZespolowyContext(DbContextOptions<ProjektZespolowyContext>options)
+            : base(options)
+        {
+
+        }
     }
 }
