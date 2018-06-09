785f4220604e38245f55ac3cb080132504fdfb8f
GZych <zbchcz@gmail.com>
2018-06-10 00:33:29 +0200

    Dodano metodę zapisu Rachunku i nowy konstruktor do Context(Może nie działać)

---
+++ WebApplication6/Migrations/20180609221453_m13.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ProjektZespolowy.Migrations
+{
+    public partial class m13 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FakturaId",
+                table: "Rachunki",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Paragon",
+                table: "Rachunki",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rachunki_FakturaId",
+                table: "Rachunki",
+                column: "FakturaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rachunki_Faktury_FakturaId",
+                table: "Rachunki",
+                column: "FakturaId",
+                principalTable: "Faktury",
+                principalColumn: "FakturaId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rachunki_Faktury_FakturaId",
+                table: "Rachunki");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rachunki_FakturaId",
+                table: "Rachunki");
+
+            migrationBuilder.DropColumn(
+                name: "FakturaId",
+                table: "Rachunki");
+
+            migrationBuilder.DropColumn(
+                name: "Paragon",
+                table: "Rachunki");
+        }
+    }
+}
