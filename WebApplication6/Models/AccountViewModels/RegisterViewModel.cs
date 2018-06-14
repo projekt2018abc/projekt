73e0d957f29508be7ca7c239e4ba3a87c4aeec1a
Rafal KrzakRrrasdasd <krzak.rafral@gmail.com>
2018-06-14 22:39:00 +0200

    Zmieniony formularz rejestracji

--- WebApplication6/Models/AccountViewModels/RegisterViewModel.cs
+++ WebApplication6/Models/AccountViewModels/RegisterViewModel.cs
@@ -8,21 +8,53 @@ namespace WebApplication6.Models.AccountViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Pole wymagane")]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "{0} musi mieć przynajmniej {2}, a maksymalnie {1} znaków.", MinimumLength = 6)]
+        [Required(ErrorMessage = "Pole wymagane")]
+        [StringLength(100, ErrorMessage = "{0} musi zawierać od {2} do {1} znaków.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Hasło")]
         public string Password { get; set; }
 
+        [Required(ErrorMessage = "Pole wymagane")]
         [DataType(DataType.Password)]
-        [Display(Name = "Potwierdź hasło")]
-        [Compare("Password", ErrorMessage = "Podane hasła nie pasują do siebie")]
+        [Display(Name = "Powtórz hasło")]
+        [Compare("Password", ErrorMessage = "Wprowadzone hasła różnią się.")]
         public string ConfirmPassword { get; set; }
+
+        [RegularExpression(@"^[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+$", ErrorMessage = "Dozwolone tylko litery")]
+        [Required(ErrorMessage = "Pole wymagane")]
+        [Display(Name = "Imie")]
+        public string FirstName { get; set; }
+
+        [RegularExpression(@"^[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+$", ErrorMessage = "Dozwolone tylko litery")]
+        [Required(ErrorMessage = "Pole wymagane")]
+        [Display(Name = "Nazwa Firmy")]
+        public string CompanyName { get; set; }
+
+        [RegularExpression(@"^[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+$", ErrorMessage = "Dozwolone tylko litery")]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Pole wymagane")]
+        [Display(Name = "Nazwisko")]
+        public string LastName { get; set; }
+
+        [StringLength(11, ErrorMessage = "Numer PESEL ma 11 cyfr.", MinimumLength = 11)]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Numer PESEL może zawierać tylko cyfry")]
+        [Display(Name = "PESEL")]
+        public string Pesel { get; set; }
+
+        [Display(Name = "NIP")]
+        [StringLength(10, ErrorMessage = "Numer NIP ma 10 cyfr.", MinimumLength = 10)]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Numer NIP może zawierać tylko cyfry")]
+        public string Nip { get; set; }
+
+        [Display(Name = "REGON")]
+        [StringLength(14, ErrorMessage = "Numer REGON ma 9 lub 14 cyfr.", MinimumLength = 9)]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Numer REGON może zawierać tylko cyfry")]
+        public string Regon { get; set; }
 
         [Required]
         [Display(Name ="Rodzaj Konta")]
